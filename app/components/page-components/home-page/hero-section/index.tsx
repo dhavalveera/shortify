d213eb4de601fe8cff87a5ad662f8d0b745f0729
Dhaval Vira <dhavalveera@gmail.com>
2024-03-02 16:23:10 +0530

    small change in hero section'

--- app/components/page-components/home-page/hero-section/index.tsx
+++ app/components/page-components/home-page/hero-section/index.tsx
@@ -3,14 +3,11 @@ import type { FC } from 'react'
 // Common
 import LinkButton from '~/components/common/link-button'
 
-// UI
-import { MdTwoColXsOneCol } from '~/ui'
-
 const HomeHeroSection: FC = () => {
   return (
-    <div className="py-10">
-      <div className={MdTwoColXsOneCol}>
-        <div>
+    <section className="body-font text-gray-600">
+      <div className="mx-auto flex flex-col items-center py-24 md:flex-row md:px-5">
+        <div className="mb-16 flex flex-col items-center text-center md:mb-0 md:w-1/2 md:items-start md:pr-16 md:text-left lg:flex-grow lg:pr-24">
           <h1 className="text-4xl font-extrabold text-[#252628] md:text-6xl">
             Build superior digital <span className="text-blue-700">connections.</span>
           </h1>
@@ -20,10 +17,15 @@ const HomeHeroSection: FC = () => {
             fingertips with <strong className="underline">Shortify</strong>.
           </p>
 
-          <LinkButton linkHref="/auth/signup" linkLabel="Register now" btnClass="w-full md:w-fit capitalize" />
+          <div className="flex justify-center">
+            <LinkButton linkHref="/auth/signup" linkLabel="Register now" btnClass="w-full md:w-fit capitalize" />
+          </div>
+        </div>
+        <div className="w-5/6 md:w-1/2 lg:w-full lg:max-w-lg">
+          <img className="rounded object-cover object-center" alt="hero" src="https://dummyimage.com/720x600" />
         </div>
       </div>
-    </div>
+    </section>
   )
 }
 
